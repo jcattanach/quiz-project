4ff7a648d574c286b20757cf4a0d3f7a103634d8
Jack Cattanach <jackcattanach@live.com>
2018-10-13 14:39:04 -0500

    added multiple choice feature and basic css

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ ideas/desired features:
 3.  gives them an option to either print their quiz or create an id to give to students, who could log on   and take the test on the website, in which case the website grades the test/quiz and saves that info and gives it back to the teacher.
 4.  give the admin the ability to save the quizzes and edit them later
 5.  ability to create different formats (multiple choice, etc)
+6.  ability to put a timer on the quiz when the students are taking it.
--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let textboxQuizName = document.getElementById('textboxQuizName')
 let btnSubmitQuizName = document.getElementById('btnSubmitQuizName')
 let headerQuizName = document.getElementById('headerQuizName')
 let listQuestionAndAnswer = document.getElementById('listQuestionAndAnswer')
+let addMultipleChoiceQuestion = document.getElementById('addMultipleChoiceQuestion')
 
 function deleteQuestionFunction(listItem) {
   whichList = listItem.parentElement
@@ -11,18 +12,36 @@ function deleteQuestionFunction(listItem) {
 
 enterQuestionAndAnswer = `
 <li>
-  <input type="text" id="quizQuestion" placeholder="Enter the question"/>
-  <input type="text" id="quizAnswer" placeholder="Enter the answer" required/>
+  <input type="text" id="quizQuestion" placeholder="Question"/>
+  <input type="text" id="quizAnswer" placeholder="Answer" required/>
+  <input type="text" id="quizWrongAnswer" placeholder="Alternative answer (optional)" />
+  <input type="text" id="quizWrongAnswer" placeholder="Alternative answer (optional)" />
+  <input type="text" id="quizWrongAnswer" placeholder="Alternative answer (optional)" />
   <button id="deleteQuestion" onclick="deleteQuestionFunction(this.parentElement)">remove question</button>
-
 </li>`
 
+enterMultipleChoice = `
+<li>
+  <input type="text" id="multipleChoiceQuestion" placeholder="Question"/>
+  <input type="checkbox" id="checkboxMultipleChoiceAnswerOne"/>
+  <input type="text" id="multipleChoiceAnswerOne" placeholder="Answer" />
+  <input type="checkbox" id="checkboxMultipleChoiceAnswerTwo"/>
+  <input type="text" id="multipleChoiceAnswerTwo" placeholder="Answer" />
+  <input type="checkbox" id="checkboxMultipleChoiceAnswerThree"/>
+  <input type="text" id="multipleChoiceAnswerThree" placeholder="Answer" />
+  <input type="checkbox" id="checkboxMultipleChoiceAnswerFour"/>
+  <input type="text" id="multipleChoiceAnswerFour" placeholder="Answer" />
+  <button id="deleteQuestion" onclick="deleteQuestionFunction(this.parentElement)">remove question</button>
+</li>`
 //event listeners
+
 btnSubmitQuizName.addEventListener('click', function() {
   quizName = textboxQuizName.value
   headerQuizName.innerHTML = quizName
 })
 addQuestion.addEventListener('click', function() {
-
     listQuestionAndAnswer.insertAdjacentHTML('beforeend', enterQuestionAndAnswer)
+})
+addMultipleChoiceQuestion.addEventListener('click', function() {
+    listQuestionAndAnswer.insertAdjacentHTML('beforeend', enterMultipleChoice)
 })
