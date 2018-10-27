19dbf7c7ee48eb5e3e549bceebb4109ec0e306cb
Eslam Amin <eslamamin@MacBook-Air-1742.local>
2018-10-27 18:00:34 -0500

    show results

---
+++ results.js
@@ -0,0 +1,73 @@
+let testContainer = document.getElementById("testContainer")
+let testsList = document.getElementById("testsList")
+let resultsContainer = document.getElementById("resultsContainer")
+let resultsList = document.getElementById("resultsList")
+let viewTestsBtn = document.getElementById("viewTestsBtn")
+const database = firebase.database()
+const testsRef = database.ref("Tests")
+const usersRef =  database.ref("Users")
+
+let currentJSUser = localStorage.getItem("vCurrentUser")
+let currentUserID = currentJSUser
+
+firebase.auth().onAuthStateChanged(function(user) {
+ if (user) {
+   viewTestsBtn.addEventListener('click', function () {
+  usersRef.on('value',function(snapshot){
+      testsList.innerHTML = ''
+      snapshot.forEach(function(childSnapshot){
+        if (childSnapshot.key == currentUserID){
+        childSnapshot.forEach(function(childChildSnapshot){
+          if (childChildSnapshot.key == "Tests"){
+            childChildSnapshot.forEach(function(childChildChildSnapshot){
+              key = childChildChildSnapshot.key
+              value = childChildChildSnapshot.val()
+        testsList.innerHTML += `<li><a href="#" onclick="javascript:showResults()">${value}</a> ${key}</li>`
+      })
+      }
+      })
+    }
+      })
+    })
+  })
+}
+})
+
+function showResults(){
+   firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+     usersRef.on('value',function(snapshot){
+         resultsList.innerHTML = ''
+         snapshot.forEach(function(childSnapshot){
+           if (childSnapshot.key == currentUserID){
+           childSnapshot.forEach(function(childChildSnapshot){
+             if (childSnapshot.val().AccountType == "Teacher" ){
+               childChildSnapshot.forEach(function(childChildChildSnapshot){
+                 console.log(childSnapshot.val().Students)
+                 const yourStudents = childSnapshot.val().Students
+                 Object.keys(yourStudents).forEach(studentKey => {
+                       studentObj = yourStudents[studentKey]
+                       Object.keys(studentObj.Tests).forEach(studentTestId => {
+                           const studentTestScore = studentObj.Tests[studentTestId]
+                           resultsList.innerHTML += `<li>Test ID:${studentTestId} Score: ${studentTestScore}</li>`
+                       })
+                   })
+                 key = childChildChildSnapshot.key
+                 score = childChildChildSnapshot.val()
+             })
+            }
+          })
+         }
+       })
+     })
+   }
+ })
+}
+
+let logOutButton = document.getElementById("logOutButton")
+
+logOutButton.addEventListener('click', function () {
+currentUserID = ""
+  localStorage.setItem("vCurrentUser", currentUserID)
+  document.location.href = "index.html"
+})
